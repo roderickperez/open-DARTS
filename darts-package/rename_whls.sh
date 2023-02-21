8aca6d9da5e86df597aa881405e2646f4639156a
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-02-21 18:27:21 +0100

    rename whls script added

--- darts-package/rename_whls.sh
+++ darts-package/rename_whls.sh
@@ -1,3 +1,4 @@
+# rename whl: replace the "linux" suffix with "manylinux2014" to make uploadable to pypi
 for fname in `ls ./dist/*whl`
 do
     newfname=`echo $fname | sed "s|linux|manylinux2014|g"`
