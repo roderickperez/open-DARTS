34ce03c5c0947d7c9b9a2c44acb1aa4e1284f7d8
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-03-02 15:21:44 +0100

    package name fixed for batch; removed engines_build_info.cpp

--- darts-engines/src/engines_build_info.cpp
+++
@@ -1,4 +0,0 @@
-#include "engines_build_info.h"
-const char *ENGINES_BUILD_DATE = "15/02/2023, 13:41:56";
-const char *ENGINES_BUILD_MACHINE = "isaifullin@griend.ta.tudelft.nl";
-const char *ENGINES_BUILD_GIT_HASH = "77344ca-dirty";
