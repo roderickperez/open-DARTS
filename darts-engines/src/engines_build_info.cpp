2dbbcf8885e0a77a3890cfcf8a9bc51696358e23
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-03-04 09:26:44 +0100

    unused files removed from the repository

--- darts-engines/src/engines_build_info.cpp
+++
@@ -1,4 +0,0 @@
-#include "engines_build_info.h"
-const char* ENGINES_BUILD_DATE = "do 23-02-2023 23:08:09"; 
-const char*  ENGINES_BUILD_MACHINE = "dvoskov@TUD261133"; 
-const char*  ENGINES_BUILD_GIT_HASH = "342c234-dirty"; 
