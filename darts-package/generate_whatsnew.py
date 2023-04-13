9f48d831653d7ec05083056700e96c0889d5f289
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-04-13 15:10:42 +0200

    added python script to generate xls file with message list

---
+++ darts-package/generate_whatsnew.py
@@ -0,0 +1,29 @@
+import git
+from openpyxl import Workbook
+
+# works from current folder, so synchronize it first
+repo = git.Repo("..")
+
+ver = '0.1.3'  # previous release version
+branch = 'development'  #  branch for processing
+
+# commit which corresponds to previous release
+end_commit = repo.commit(repo.tags[ver])
+
+wb = Workbook()
+ws = wb.active  # get active worksheet
+
+commits_list = list(repo.iter_commits(branch))
+# loop by commits, fill the worksheet
+for commit in commits_list:
+    # columns of worksheet: date, author, commit_message
+    commit_data = []
+    commit_data.append(commit.committed_datetime.date())
+    commit_data.append(commit.author.name)
+    commit_data.append(commit.message)
+    if commit == end_commit:
+        break
+    ws.append(commit_data)  # add row to worksheet
+
+# Save the file
+wb.save("whatsnew.xlsx")
