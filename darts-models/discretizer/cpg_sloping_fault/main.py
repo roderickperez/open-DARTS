5fd1aecbef1dd5a6ba1667c7aacc39d9ad4efb8a
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-05-03 17:21:14 +0200

    test case with cpg grid added

---
+++ darts-models/discretizer/cpg_sloping_fault/main.py
@@ -0,0 +1,208 @@
+from darts.engines import value_vector, redirect_darts_output, sim_params, timer_node
+
+import os, sys, inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+darts_dir = os.path.dirname(os.path.dirname(current_dir))  # 2 levels up
+model_dir = os.path.join(darts_dir, '2ph_do')
+sys.path.insert(0, model_dir)
+from model_cpg import Model
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import time
+
+import os
+
+from cpg_tools import save_array, make_full_cube
+
+def run(discr_type : str, gridfile : str, propfile : str, sch_fname : str,
+        dt : float, n_time_steps : int,
+        export_vtk=False,
+        only_1st_layer=False):
+    model_suffix = gridfile.split('/')[-2]
+    model_dir = os.path.dirname(gridfile)
+    vtk_filename = 'Result_' + model_suffix + '_' + discr_type
+    print('Test started.', 'discr_type:', discr_type)
+    redirect_darts_output(os.path.join(model_dir, 'out_' + model_suffix + '_' + discr_type + '.log'))
+    m = Model(discr_type=discr_type, gridfile=gridfile, propfile=propfile, sch_fname=sch_fname)
+
+    # debug
+    if discr_type == 'cpp':
+        tran_fname = os.path.join(model_dir, 'tran_cpg.grdecl')
+        nnc_fname = os.path.join(model_dir, 'nnc.txt')
+        m.reservoir.discr.write_tran_cube(tran_fname, nnc_fname)
+
+    # add wells
+    if False:
+        m.add_wells(mode='read', sch_fname=sch_fname, verbose=True)#, well_index=1000) # add only perforations
+        m.set_wells() # set well controls - for dap (case_201) only
+    else:
+        m.add_wells(mode='generate', sch_fname=sch_fname)
+
+    m.init()
+    if export_vtk:
+        m.export_pro_vtk(vtk_filename)
+    m.params.max_ts = dt
+    m.save_cubes(os.path.join(model_dir, 'p_0')) # debug
+
+    t = 0
+    #print_range(m, t)
+
+    for ti in range(n_time_steps):
+        m.run_python(dt)
+
+        #print_range(m, t)
+        t += dt
+
+        if export_vtk:
+            m.export_pro_vtk(vtk_filename)
+
+        #m.save_cubes(os.path.join(model_dir, 'p_' + str(ti+1)), arr, arr_names)
+ 
+    m.print_timers()
+    m.print_stat()
+
+    time_data = pd.DataFrame.from_dict(m.physics.engine.time_data)
+    time_data.to_pickle(os.path.join(model_dir, 'darts_time_data_' + discr_type + '.pkl'))
+
+    # filter data and write to xlsx
+    # list the column names that should be removed
+    press_gridcells = time_data.filter(like='reservoir').columns.tolist()
+    chem_cols = time_data.filter(like='Kmol').columns.tolist()
+    # remove columns from data
+    time_data.drop(columns=press_gridcells + chem_cols, inplace=True)
+    # add time in years
+    time_data['Time (years)'] = time_data['time'] / 365.25
+    writer = pd.ExcelWriter(os.path.join(model_dir, 'time_data_' + discr_type + '.xlsx'))
+    time_data.to_excel(writer, 'Sheet1')
+    writer.close()
+
+    return time_data
+
+# calculate total rate (sum over the wells) and make combined plot for 2 results
+# darts_df_1, darts_df_2 - two results (dataframes)
+# search_str - what to plot (oil rate, water rate,..)
+# plot_names - list to use as legend, length should be =2
+# prod=True -> production, False -> injection
+def plot_total_rate_darts(darts_df_1, darts_df_2, opm_df, opm_smry, plot_names, search_str, mode_suffix):
+    acc_df = pd.DataFrame()
+    acc_df['time'] = darts_df_1['time']
+    name_plot_1, name_plot_2 = plot_names[0], plot_names[1]
+    acc_df[name_plot_1] = 0
+    acc_df[name_plot_2] = 0
+    for col in darts_df_1.columns:
+        if search_str in col:
+            if (mode_suffix == 'prod' and 'INJ' not in col) or (mode_suffix != 'prod' and 'INJ' in col):
+                acc_df[name_plot_1] += darts_df_1[col]
+                acc_df[name_plot_2] += darts_df_2[col]
+    acc_df[name_plot_1] = acc_df[name_plot_1].abs()
+    acc_df[name_plot_2] = acc_df[name_plot_2].abs()
+    ax = None
+    ax = acc_df.plot(x='time', y=name_plot_1, style='.-', color='r', ax=ax, alpha=1)
+    ax = acc_df.plot(x='time', y=name_plot_2, style='-.', color='b', ax=ax, alpha=1)
+    if opm_df is not None:
+        ax = opm_df.plot(x='days', y=opm_smry, style='.-.', color='g', ax=ax, alpha=1)
+    ymin, ymax = ax.get_ylim()
+    if ymax < 0:
+        ax.set_ylim(ymin * 1.1, 0)
+    if ymin > 0:
+        ax.set_ylim(0, ymax * 1.1)
+    #plt.show(block=False)
+    return ax
+
+def plot(time_data_1, time_data_2, plot_names, opm_csv_fname=None):
+    plt.rc('font', size=12)
+    plt_list = [#(': oil rate',   'prod', 'OPM_FOPR'),
+                (': water rate', 'prod', 'OPM_FWPR'),
+                (': water rate', 'inj',  'OPM_FWIR')]
+
+    opm_df = None
+    if opm_csv_fname is not None:
+        opm_df = pd.read_csv(opm_csv_fname, delimiter='\t')
+
+    counter = 1
+    for (plt_str, mode_suffix, opm_smry) in plt_list:
+        #plt.subplot(len(plt_list), 1, counter)
+        ax1 = plot_total_rate_darts(time_data_1, time_data_2, opm_df, opm_smry,
+                                    plot_names, plt_str, mode_suffix)
+        title = mode_suffix + ' ' + plt_str[2:] + ', sm$^3$/day'
+        ax1.set(xlabel="Days", ylabel=title, title=title)
+        #counter += 1
+        #plt.show()
+        model_suffix = gridfile.split('/')[-2]
+        model_dir = os.path.dirname(gridfile)
+        png_fname = os.path.join(model_dir, mode_suffix + '_' + plt_str[2:] + '.png')
+        plt.savefig(png_fname)
+        print('File', png_fname, 'saved')
+
+#####################################################
+def get_case_files(case):
+    prefix = r'meshes/case_' + str(case)
+    gridfile = prefix + '/grid.grdecl'
+    propfile = prefix + '/reservoir.in'
+
+    sch_dir = os.path.join(prefix, 'opm_flow_model')
+    sch_file = os.path.join(sch_dir, 'SCH.INC')
+    if not os.path.exists(sch_dir):
+        os.mkdir(sch_dir)
+    return gridfile, propfile, sch_file
+
+
+#####################################################
+if __name__ == '__main__':
+
+    # 3 years by 1 month
+    dt = 30
+    n_time_steps = 12*3
+
+    export_vtk = False # slow for big models
+
+    #dt = 10
+    #n_time_steps = 1
+
+    discr_types_list = ['cpp'] #cpg
+    #discr_types_list = ['python'] #struct
+    #discr_types_list = ['cpp', 'python']
+
+    # small grids
+    cases_list = [43] # 10x10x10 sloping fault
+
+    # run all the  grids cases
+    #cases_list = []
+    #for c in os.listdir('../../meshes/cpg/'):
+    #    if c.startswith("Case_"):
+    #        cases_list.append(c)
+
+    print('cases_list:', cases_list)
+
+    mode = 'run'      # run and plot results
+    #mode = 'compare' # plot results
+    print('MODE', mode)
+
+    results = dict()
+
+    for case in cases_list:
+        gridfile, propfile, sch_fname = get_case_files(case)
+        prefix = r'./meshes/case_' + str(case)
+        if mode == 'run':
+            for discr_type in discr_types_list:
+                start = time.perf_counter()
+                results[discr_type] = run(discr_type=discr_type, gridfile=gridfile, propfile=propfile,
+                                          sch_fname=sch_fname, dt=dt, n_time_steps=n_time_steps,
+                                          export_vtk=export_vtk)
+                end = time.perf_counter()
+        elif mode == 'compare':
+            for discr_type in discr_types_list:
+                results[discr_type] = pd.read_pickle(prefix + '/darts_time_data_' + discr_type + '.pkl')
+
+        opm_fname = prefix + '/opm_flow_model/opm_rates.csv'
+        if not os.path.exists(opm_fname):
+            opm_fname = None
+
+        if len(discr_types_list) > 1 and not results[discr_types_list[0]].empty and not results[discr_types_list[1]].empty:
+            plot(results[discr_types_list[0]], results[discr_types_list[1]], discr_types_list, opm_fname)
+        else:
+            # if only one case did run, then plot it twice, because need to pass 2-nd arg to plot function
+            plot(results[discr_types_list[0]], results[discr_types_list[0]], discr_types_list + ['None'], opm_fname)
+            print('case', case, ': Skipped plotting due to some of data absence!')
---
+++ darts-models/discretizer/cpg_sloping_fault/model_cpg.py
@@ -0,0 +1,204 @@
+import numpy as np
+import time
+
+from reservoir import CPG_Reservoir
+from darts.discretizer import load_single_float_keyword, load_single_int_keyword
+from darts.discretizer import value_vector as value_vector_discr
+from darts.discretizer import index_vector as index_vector_discr
+from darts.engines import value_vector
+
+from darts.models.reservoirs.struct_reservoir import StructReservoir
+from darts.tools.keyword_file_tools import save_few_keywords
+from cpg_tools import save_array
+
+# inherit from darts-models/2ph_do model to use its physics; self.reservoir will be replaced in this file
+# add path to import
+import os, sys, inspect
+current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+darts_dir = os.path.dirname(os.path.dirname(current_dir))  # 2 levels up
+model_dir = os.path.join(darts_dir, '2ph_do')
+sys.path.insert(0, model_dir)
+from model import Model as BaseModel
+
+class Model(BaseModel):
+    def __init__(self, discr_type='cpp', gridfile='', propfile='', sch_fname='', n_points=1000):
+        # measure time spend on reading/initialization
+        #self.timer.node["initialization"].start()
+        # call base class constructor
+        super().__init__()
+        self.n_points = n_points
+
+        self.discr_type = discr_type
+        if discr_type == 'cpp':
+            self.reservoir = CPG_Reservoir(gridfile, propfile)
+        elif discr_type == 'python':
+            self.init_struct_rsv()
+
+        #self.timer.node["initialization"].stop()
+    def init_struct_rsv(self):
+        self.dims_cpp = index_vector_discr()
+        load_single_int_keyword(self.dims_cpp, gridfile, "SPECGRID", 3)
+        self.dims = np.array(self.dims_cpp, copy=False)
+
+        self.permx_cpp, self.permy_cpp, self.permz_cpp = value_vector_discr(), value_vector_discr(), value_vector_discr()
+        load_single_float_keyword(self.permx_cpp, propfile, 'PERMX', -1)
+        load_single_float_keyword(self.permy_cpp, propfile, 'PERMY', -1)
+        self.permx = np.array(self.permx_cpp, copy=False)
+        self.permy = np.array(self.permy_cpp, copy=False)
+        for perm_str in ['PERMEABILITYXY', 'PERMEABILITY']:
+            if self.permx.size == 0 or self.permy.size == 0:
+                load_single_float_keyword(self.permx_cpp, propfile, perm_str, -1)
+                self.permy_cpp = self.permx_cpp
+                self.permx = np.array(self.permx_cpp, copy=False)
+                self.permy = np.array(self.permy_cpp, copy=False)
+        load_single_float_keyword(self.permz_cpp, propfile, 'PERMZ', -1)
+        self.permz = np.array(self.permz_cpp, copy=False)
+
+        self.poro_cpp = value_vector_discr()
+        load_single_float_keyword(self.poro_cpp, propfile, 'PORO', -1)
+        self.poro = np.array(self.poro_cpp, copy=False)
+
+        self.coord_cpp = value_vector_discr()
+        load_single_float_keyword(self.coord_cpp, gridfile, 'COORD', -1)
+        self.coord = np.array(self.coord_cpp, copy=False)
+
+        self.zcorn_cpp = value_vector_discr()
+        load_single_float_keyword(self.zcorn_cpp, gridfile, 'ZCORN', -1)
+        self.zcorn = np.array(self.zcorn_cpp, copy=False)
+
+        self.actnum_cpp = index_vector_discr()
+        self.actnum = np.array([])
+        for fname in [gridfile, propfile]:
+            if self.actnum.size == 0:
+                load_single_int_keyword(self.actnum_cpp, fname, 'ACTNUM', -1)
+                self.actnum = np.array(self.actnum_cpp, copy=False)
+        if self.actnum.size == 0:
+            self.actnum = np.ones(self.dims[0] * self.dims[1] * self.dims[2])
+            print('No ACTNUM found in input files. ACTNUM=1 will be used')
+
+        self.depth = 0
+        self.dx, self.dy, self.dz = [], [], []
+        self.dx, self.dy, self.dz = 0, 0, 0
+
+        # make cells with zero porosity (make sense if not thermal)
+        # self.actnum[self.poro == 0.0] = 0
+
+        # makes sense for thermal
+        self.poro = np.array(self.reservoir.mesh.poro, copy=False)
+        self.poro[self.poro == 0.0] = 1.E-4
+
+        self.reservoir = StructReservoir(self.timer, nx=self.dims[0], ny=self.dims[1], nz=self.dims[2],
+                                         dx=self.dx, dy=self.dy, dz=self.dz,
+                                         permx=self.permx, permy=self.permy, permz=self.permz, poro=self.poro,
+                                         depth=self.depth, actnum=self.actnum, coord=self.coord, zcorn=self.zcorn,
+                                         is_cpg=True)
+
+    def set_initial_conditions(self):
+        self.physics.set_uniform_initial_conditions(self.reservoir.mesh, uniform_pressure=200, uniform_composition=[0.999])
+        #self.set_initial_pressure_from_file(self.gridfile)
+
+        # save initial array for geomech
+        p_mesh = np.array(self.reservoir.mesh.pressure, copy=True)
+        self.p_initial = p_mesh[:self.reservoir.mesh.n_res_blocks * 2] # nvars=2
+        #TODO save t_initial
+
+    def set_initial_pressure_from_file(self, fname):
+        # set initial pressure
+        p_cpp = value_vector()
+        load_single_float_keyword(p_cpp, fname, 'PRESSURE', -1)
+        p_file = np.array(p_cpp, copy=False)
+
+        p_mesh = np.array(self.reservoir.mesh.pressure, copy=False)
+        try:
+            actnum = np.array(self.reservoir.actnum, copy=False) # CPG Reservoir
+            #nb = self.reservoir.mesh.n_cells
+        except:
+            actnum = self.reservoir.global_data['actnum']  #Struct reservoir
+        nb = self.reservoir.mesh.n_blocks
+        p_mesh[:self.reservoir.mesh.n_res_blocks * 2] = p_file[actnum > 0]
+
+    def set_boundary_conditions(self):
+        for i, w in enumerate(self.reservoir.wells):
+            if "INJ" in w.name:
+                w.control = self.physics.new_bhp_inj(250, self.inj)
+            else:
+                w.control = self.physics.new_bhp_prod(100)
+
+    def add_wells(self, mode='generate', sch_fname=None, well_index=-1, verbose=False):
+        self.read_and_add_perforations(sch_fname, well_index=well_index, verbose=verbose)
+
+    def set_boundary_conditions(self):
+        for i, w in enumerate(self.reservoir.wells):
+            if "INJ" in w.name:
+                w.control = self.physics.new_bhp_inj(250, value_vector([0.999]))
+            else:
+                w.control = self.physics.new_bhp_prod(100)
+
+
+    #TODO: combine this function with save_few_keywords
+    def save_cubes(self, fname, arr_list = [], arr_names = []):
+        '''
+        arr - list of numpy arrays to save, size=nactive
+        arr_names - list of array names (keyword)
+        '''
+        Xn = np.array(self.physics.engine.X, copy=False)
+        P = Xn[0:self.reservoir.mesh.n_res_blocks * 2:2]
+        try:
+            actnum = np.array(self.reservoir.actnum, copy=False)  # CPG Reservoir doesn't have 'global_data' object
+            suffix = 'cpg'
+        except:
+            actnum = self.reservoir.global_data['actnum']  # Struct Reservoir
+            suffix = 'struct'
+        fname_suf = fname + '_' + suffix + '.grdecl'
+
+        arr_list += [P]
+        arr_names += 'PRESSURE'
+
+        save_array(actnum, fname_suf, 'ACTNUM', actnum, 'w')
+        for i in range(len(arr_list)):
+            save_array(arr_list[i], fname_suf, arr_names[i], actnum, 'a')
+
+    def read_and_add_perforations(self, sch_fname, well_index=-1, verbose=False):
+        '''
+        read COMPDAT from SCH file in Eclipse format, add wells and perforations
+        note: uses only I,J,K1,K2 parameters from COMPDAT
+        '''
+        if sch_fname is None:
+            return
+        print('reading wells (COMPDAT) from', sch_fname)
+        well_dia = 0.152
+        well_rad = well_dia / 2
+
+        keep_reading = True
+        prev_well_name = ''
+        with open(sch_fname) as f:
+            while keep_reading:
+                buff = f.readline()
+                if 'COMPDAT' in buff:
+                    while True:  # be careful here
+                        buff = f.readline()
+                        if len(buff) != 0:
+                            CompDat = buff.split()
+                            wname = CompDat[0].strip('"').strip("'") #remove quotas (" and ')
+                            if len(CompDat) != 0 and '/' != wname:  # skip the empty line and '/' line
+                                # define well
+                                if wname == prev_well_name:
+                                    pass
+                                else:
+                                    self.reservoir.add_well(wname)
+                                    prev_well_name = wname
+                                # define perforation
+                                i1 = int(CompDat[1])
+                                j1 = int(CompDat[2])
+                                k1 = int(CompDat[3])
+                                k2 = int(CompDat[4])
+                                for i in range(k1, k2 + 1):
+                                    self.reservoir.add_perforation(self.reservoir.wells[-1],
+                                                                   i1, j1, i,
+                                                                   well_radius=well_rad, well_index=well_index,
+                                                                   multi_segment=False, verbose=verbose)
+
+                            if len(CompDat) != 0 and '/' == CompDat[0]:
+                                keep_reading = False
+                                break
+        print('WELLS read from SCH file:', len(self.reservoir.wells))
