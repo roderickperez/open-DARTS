84722f5c57142383d46ec21406a40b7c9a0a29a7
Denis Voskov <d.v.voskov@tudelft.nl>
2023-06-16 11:47:16 +0000

    Update build_install_darts.bat

--- build_install_darts.bat
+++ build_install_darts.bat
@@ -1,3 +1,3 @@
 python setup.py clean
 python setup.py build bdist_wheel
-python -m pip install --upgrade --no-deps --force-reinstall dist/open_darts-0.1.4-cp39-cp39-win_amd64.whl
+python -m pip install --upgrade --no-deps --force-reinstall dist/open_darts-1.0.0-cp39-cp39-win_amd64.whl
