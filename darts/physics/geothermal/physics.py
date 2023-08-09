4d67f9e52a322400e4e22020a4c189eb51407d02
Ilshat Saifullin <I.S.Saifullin@tudelft.nl>
2023-08-09 11:56:50 +0200

    added missing import for geothermal

--- darts/physics/geothermal/physics.py
+++ darts/physics/geothermal/physics.py
@@ -1,5 +1,5 @@
 from darts.engines import *
-
+from darts.physics.properties.iapws.iapws_property import *
 from darts.physics.physics_base import PhysicsBase
 from darts.physics.geothermal.operator_evaluator import *
 
