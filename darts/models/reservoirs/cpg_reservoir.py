92e09ee7270fb0e7f559a574e4690472ddc5a316
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2023-08-10 10:04:15 +0200

    Change reservoir.nb to mesh.n_res_blocks

--- darts/models/reservoirs/cpg_reservoir.py
+++ darts/models/reservoirs/cpg_reservoir.py
@@ -159,7 +159,7 @@ class CPG_Reservoir:
         self.nx = self.discr_mesh.nx = self.dims[0]
         self.ny = self.discr_mesh.ny = self.dims[1]
         self.nz = self.discr_mesh.nz = self.dims[2]
-        self.nb = self.nx * self.ny * self.nz
+        self.nb = self.mesh.n_res_blocks
         self.discr_mesh.n_cells = ugrid.number_of_cells
         # cells + boundary_faces, approximate
         self.discr_mesh.num_of_elements = self.discr_mesh.n_cells + \
