ee0d51e3493b3810697f1f7911f2826866a7e913
Ilshat Saifullin <i.s.saifullin@tudelft.nl>
2023-08-24 17:46:27 +0000

    Build - Added "debug" argument to build_scripts. Number of threads moved to the third argument in build_darts.sh and build_darts.bat How to use: `build_darts.sh ODLS config cores` Example: `build_darts.sh 1 debug 8`
    Pipeline with iterative solvers run for non-protected branches too:
    - py3.9: run non-ODLS for merge_requests (non-protected branches); before it was only for ODLS configuration 
    - allow to run non-protected branches if TEST_ALL=1 variable defined (for running all python versions)
    
    Pipelines with UPLOAD_PKL=1:
    - only updated pkl will be added to the archive
    - windows: also add pkl from 'ref' subfolder

--- models/upload_pkl.sh
+++ models/upload_pkl.sh
@@ -7,10 +7,16 @@ if [ "$UPLOAD_PKL" != 1 ]; then
 	exit
 fi
 
-fname="$commit"_"$odls"_"$py.tar.gz"
+fname="$commit"_ODLS"$odls"_PY"$py.tar.gz"
 echo $fname
 
-tar -czf $fname ./*/*.pkl ./*/ref/*.pkl
+pklname="pkl_lin"
+if [ $odls == "0" ]
+then
+    pklname=$pklname"_iter"
+fi
+
+tar -czf $fname ./*/"$pklname".pkl ./*/ref/"$pklname".pkl
 
 smbclient -U $2%$3 //$1/darts-private-artifacts -c "put $fname" -D=pkl
 
