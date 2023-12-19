b45efc507c667e40db13852c051000a38cf762e0
Michiel Wapperom <m.b.wapperom@tudelft.nl>
2023-12-19 14:38:43 +0100

    Call DartsModel run() method instead of engine.run()

--- models/Adjoint_super_engine/model_definition.py
+++ models/Adjoint_super_engine/model_definition.py
@@ -36,7 +36,7 @@ class Model(CICDModel, OptModuleSettings):
         self.set_physics()
 
         self.set_sim_params(first_ts=0.001, mult_ts=2, max_ts=1, runtime=1000,
-                            tol_newton=1e-6, tol_linear=1e-3, it_newton=10, it_linear=50,
+                            tol_newton=1e-2, tol_linear=1e-3, it_newton=10, it_linear=50,
                             newton_type=sim_params.newton_local_chop)
 
         self.timer.node["initialization"].stop()
@@ -183,7 +183,7 @@ class Model(CICDModel, OptModuleSettings):
                 else:
                     w.control = self.physics.new_bhp_prod(50)
 
-            self.engine.run(ts)
+            CICDModel.run(self, ts)
             self.engine.report()
             if export_to_vtk:
                 self.export_vtk(file_name)
