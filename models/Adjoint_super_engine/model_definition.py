1ee3d4087e8741d013134292eea282d00d3397ca
mbwapperom <m.b.wapperom@tudelft.nl>
2023-12-20 09:26:15 +0100

    Change newton tolerance to 1e-6 for convergence with ODLS

--- models/Adjoint_super_engine/model_definition.py
+++ models/Adjoint_super_engine/model_definition.py
@@ -36,7 +36,7 @@ class Model(CICDModel, OptModuleSettings):
         self.set_physics()
 
         self.set_sim_params(first_ts=0.001, mult_ts=2, max_ts=1, runtime=1000,
-                            tol_newton=1e-2, tol_linear=1e-3, it_newton=10, it_linear=50,
+                            tol_newton=1e-6, tol_linear=1e-3, it_newton=10, it_linear=50,
                             newton_type=sim_params.newton_local_chop)
 
         self.timer.node["initialization"].stop()
