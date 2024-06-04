ad4a06d61a90835916748d53c0e5d12a0c0dd514
Aleks Novikov <a.novikov@tudelft.nl>
2024-06-04 18:30:35 +0200

    Renaming of run_single_newton_iteration in GPU base engine

--- engines/src/engine_base_gpu.h
+++ engines/src/engine_base_gpu.h
@@ -57,7 +57,7 @@ public:
      */
 
   /// @brief report for one newton iteration
-  virtual int run_single_newton_iteration(value_t deltat);
+  virtual int assemble_linear_system(value_t deltat);
   virtual int solve_linear_equation();
   virtual int post_newtonloop(value_t deltat, value_t time);
 
