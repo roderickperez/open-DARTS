6d543bdffb6fc133e0edb4c73fbee1b40b9338ec
Aleks Novikov <a.novikov@tudelft.nl>
2024-07-04 10:37:16 +0200

    Avoid use of override keyword for abstract virtual functions in mechanical engines

--- engines/src/mech/engine_super_elastic_cpu.hpp
+++ engines/src/mech/engine_super_elastic_cpu.hpp
@@ -105,11 +105,11 @@ public:
   // number of variables per jacobian matrix block
   const static uint8_t N_VARS_SQ = N_VARS * N_VARS;
 
-  uint8_t get_n_vars() const override { return N_VARS; };
-  uint8_t get_n_ops() const override  { return N_OPS; };
-  uint8_t get_n_comps() const override  { return NC; };
-  uint8_t get_z_var() const override  { return Z_VAR; };
-  uint8_t get_n_state() const override { return N_STATE; };
+  uint8_t get_n_vars() const { return N_VARS; };
+  uint8_t get_n_ops() const  { return N_OPS; };
+  uint8_t get_n_comps() const  { return NC; };
+  uint8_t get_z_var() const  { return Z_VAR; };
+  uint8_t get_n_state() const { return N_STATE; };
 
   engine_super_elastic_cpu()
   {
