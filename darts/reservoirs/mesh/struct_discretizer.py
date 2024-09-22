One more fix in depth calculation in structured discretizer

darts/reservoirs/mesh/struct_discretizer.py
@@ -133,9 +133,9 @@ class StructDiscretizer:
             self.centroids_all_cells = np.zeros((self.nx, self.ny, self.nz, 3)) # 3 - for x,y,z coordinates
             # fill z-coordinates using DZ
             self.centroids_all_cells[:, :, 0, 2] = start_z + self.len_cell_zdir[:, :, 0] * 0.5  # nx*ny array of current layer's depths
-            d_cumsum = self.len_cell_zdir.cumsum(axis=2)
+            distances = (self.len_cell_zdir[:, :, :-1] + self.len_cell_zdir[:, :, 1:]) * 0.5
             self.centroids_all_cells[:, :, 1:, 2] = self.centroids_all_cells[:, :, 0, 2][:, :, np.newaxis] + \
-                                                     (d_cumsum[:, :, :-1] + d_cumsum[:, :, 1:]) * 0.5
+                                                     np.cumsum(distances, axis=2)
 
             # fill y-coordinates using DY
             self.centroids_all_cells[:, 0, :, 1] = self.len_cell_ydir[:, 0, :] * 0.5  # nx*nz array