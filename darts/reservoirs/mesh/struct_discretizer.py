fix center calculation for the struct reservoir

darts/reservoirs/mesh/struct_discretizer.py
@@ -135,19 +135,19 @@ class StructDiscretizer:
             self.centroids_all_cells[:, :, 0, 2] = start_z + self.len_cell_zdir[:, :, 0] * 0.5  # nx*ny array of current layer's depths
             if nz > 1:
                 d_cumsum = self.len_cell_zdir.cumsum(axis=2)
-                self.centroids_all_cells[:, :, 1:, 2] = self.centroids_all_cells[:, :, :-1, 2] + (d_cumsum[:, :, :-1] + d_cumsum[:, :, 1:]) * 0.5
+                self.centroids_all_cells[:, :, 1:, 2] = start_z + (d_cumsum[:, :, :-1] + d_cumsum[:, :, 1:]) * 0.5
 
             # fill y-coordinates using DY
             self.centroids_all_cells[:, 0, :, 1] = self.len_cell_ydir[:, 0, :] * 0.5  # nx*nz array
             if ny > 1:
                 d_cumsum = self.len_cell_ydir.cumsum(axis=1)
-                self.centroids_all_cells[:, 1:, :, 1] = self.centroids_all_cells[:, :-1, :, 1] + (d_cumsum[:, :-1, :] + d_cumsum[:, 1:, :]) * 0.5
+                self.centroids_all_cells[:, 1:, :, 1] = (d_cumsum[:, :-1, :] + d_cumsum[:, 1:, :]) * 0.5
 
             # fill x-coordinates using DX
             self.centroids_all_cells[0, :, :, 0] = self.len_cell_xdir[0, :, :] * 0.5  # ny*nz array
             if nx > 1:
                 d_cumsum = self.len_cell_xdir.cumsum(axis=0)
-                self.centroids_all_cells[1:, :, :, 0] = self.centroids_all_cells[:-1, :, :, 0] + (d_cumsum[:-1, :, :] + d_cumsum[1:, :, :]) * 0.5
+                self.centroids_all_cells[1:, :, :, 0] = (d_cumsum[:-1, :, :] + d_cumsum[1:, :, :]) * 0.5
 
         self.perm_x_cell = self.convert_to_3d_array(permx, 'permx')
         self.perm_y_cell = self.convert_to_3d_array(permy, 'permy')