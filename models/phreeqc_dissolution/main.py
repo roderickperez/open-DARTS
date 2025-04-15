9ee95b39a32d4f650f7395a28960b4d2a71e7bd8
Aleks Novikov <a.novikov@tudelft.nl>
2025-04-15 11:49:48 +0200

    Clean up

--- models/phreeqc_dissolution/main.py
+++ models/phreeqc_dissolution/main.py
@@ -2,140 +2,8 @@ import os
 # os.environ["OMP_NUM_THREADS"] = "1"
 from model import Model
 from darts.engines import redirect_darts_output
-# from timestepping import DQNAgent
 import numpy as np
-
 from visualization import plot_profiles, animate_1d
-
-def run(self, days: float = None, restart_dt: float = 0., save_well_data: bool = True, save_solution_data: bool = True,
-        log_3d_body_path: bool = False, verbose: bool = True):
-    """
-    Method to run simulation for specified time. Optional argument to specify dt to restart simulation with.
-
-    :param days: Time increment [days]
-    :type days: float
-    :param restart_dt: Restart value for timestep size [days, optional]
-    :type restart_dt: float
-    :param verbose: Switch for verbose, default is True
-    :type verbose: bool
-    :param save_well_data: if True save states of well blocks at every time step to 'well_data.h5', default is True
-    :type save_well_data: bool
-    :param save_solution_data: if True save states of all reservoir blocks at the end of run to 'solution.h5', default is True
-    :type save_solution_data: bool
-    :param log_3d_body_path: hypercube output
-    :type verbose: bool
-    """
-    days = days if days is not None else self.runtime
-    # get current engine time
-    t = self.physics.engine.t
-    stop_time = t + days
-
-    # same logic as in engine.run
-    if np.fabs(t) < 1e-15 or not hasattr(self, 'prev_dt'):
-        dt = self.params.first_ts
-    elif restart_dt > 0.:
-        dt = restart_dt
-    else:
-        dt = min(self.prev_dt * self.params.mult_ts, days, self.params.max_ts)
-    self.prev_dt = dt
-
-    ts = 0
-
-    if log_3d_body_path:
-        self.physics.body_path_start(output_folder=self.output_folder)
-
-    # define positive-outcome and negative-outcome rewards
-    def get_reward(outcome, prev_dt, dt, LI):
-        a_ts = 100
-        a_ni = 1
-        return outcome * a_ts * np.log(dt / prev_dt) - a_ni * LI
-
-    # initial agent state
-    # prev_agent_state = np.array([0.0, 0, 0, 0, 0.0, 0.0])
-    # action = self.agent.act(prev_agent_state)
-
-    while t < stop_time:
-        converged = self.run_timestep(dt, t, verbose)
-
-        # assemble agent state
-        max_residual = np.max(self.max_residual)
-        nonlinear_iters = self.physics.engine.n_newton_last_dt
-        linear_iters = self.physics.engine.n_linear_last_dt
-        max_cfl = self.physics.engine.CFL_max
-        dX = np.asarray(self.physics.engine.dX)
-        X = np.asarray(self.physics.engine.X)
-        max_change = np.where(X != 0, dX / X, 0).max()
-        # TODO: extract time self.physics.engine.timer.get_timer()
-        agent_state = np.array([max_residual, nonlinear_iters, linear_iters, max_cfl, max_change, dt])
-
-        if converged:
-            t += dt
-            self.physics.engine.t = t
-            ts += 1
-            if verbose:
-                print("# %d \tT = %3g\tDT = %2g\tNI = %d\tLI=%d"
-                      % (ts, t, dt, self.physics.engine.n_newton_last_dt, self.physics.engine.n_linear_last_dt))
-
-            # save state-response to the agent memory
-            # reward = get_reward(outcome=1, prev_dt=self.prev_dt, dt=dt, LI=self.physics.engine.n_linear_last_dt)
-            # self.agent.remember(state=prev_agent_state, action=action, reward=reward, next_state=agent_state, done=True)
-            # self.agent.replay(self.agent_batch_size)
-
-            # new action
-            # action = self.agent.act(agent_state)
-            # dt_factor = self.agent_actions_map[action]
-            # dt = dt_factor * dt
-            dt = min(dt * self.params.mult_ts, self.params.max_ts)
-
-            # if the current dt almost covers the rest time amount needed to reach the stop_time, add the rest
-            # to not allow the next time step be smaller than min_ts
-            if np.fabs(t + dt - stop_time) < self.params.min_ts:
-                dt = stop_time - t
-
-            if t + dt > stop_time:
-                dt = stop_time - t
-            else:
-                self.prev_dt = dt
-
-            if log_3d_body_path:
-                self.physics.body_path_add_bodys(output_folder=self.output_folder, time=t)
-
-            if save_well_data:
-                self.save_data_to_h5(kind='well')
-
-        else:
-            # save state-response to the agent memory
-            # reward = get_reward(outcome=-1, prev_dt=self.prev_dt, dt=dt, LI=self.physics.engine.n_linear_last_dt)
-            # self.agent.remember(state=prev_agent_state, action=action, reward=reward, next_state=agent_state, done=True)
-            # self.agent.replay(self.agent_batch_size)
-
-            # new action
-            # action = self.agent.act(agent_state)
-            # dt_factor = self.agent_actions_map[action]
-            # dt = dt_factor * dt
-
-            dt /= self.params.mult_ts
-
-            if verbose:
-                print("Cut timestep to %2.10f" % dt)
-            assert dt > self.params.min_ts, ('Stop simulation. Reason: reached min. timestep '
-                                             + str(self.params.min_ts) + ' dt=' + str(dt))
-
-        prev_agent_state = agent_state
-
-    # update current engine time
-    self.physics.engine.t = stop_time
-
-    # save solution vector
-    if save_solution_data:
-        self.save_data_to_h5(kind='solution')
-
-    if verbose:
-        print("TS = %d(%d), NI = %d(%d), LI = %d(%d)"
-              % (self.physics.engine.stat.n_timesteps_total, self.physics.engine.stat.n_timesteps_wasted,
-                 self.physics.engine.stat.n_newton_total, self.physics.engine.stat.n_newton_wasted,
-                 self.physics.engine.stat.n_linear_total, self.physics.engine.stat.n_linear_wasted))
-    return 0
 
 def run_simulation(domain: str, max_ts: float, nx: int = 100, mesh_filename: str = None, poro_filename: str = None, output: bool = False):
     # Make a folder
@@ -179,24 +47,24 @@ def run_simulation(domain: str, max_ts: float, nx: int = 100, mesh_filename: str
     if domain == '1D':
         fig_paths = []
         fig_paths.append(plot(m))
-        run(self=m, days=0.002, restart_dt=max_ts)
+        m.run(days=0.002, restart_dt=max_ts)
         fig_paths.append(plot(m))
-        run(self=m, days=0.018, restart_dt=max_ts)
+        m.run(days=0.018, restart_dt=max_ts)
         fig_paths.append(plot(m))
-        run(self=m, days=0.02, restart_dt=max_ts)
+        m.run(days=0.02, restart_dt=max_ts)
         fig_paths.append(plot(m))
         m.params.max_ts *= 3
         m.params.first_ts = m.params.max_ts
-        run(self=m, days=0.1, restart_dt=max_ts)
+        m.run(days=0.1, restart_dt=max_ts)
         m.params.max_ts *= 4
-        run(self=m, days=0.86)
+        m.run(days=0.86)
         fig_paths.append(plot(m))
         m.params.max_ts *= 5
         m.params.first_ts = m.params.max_ts
 
         for i in range(7):
             dt = 2.0
-            run(self=m, days=dt)
+            m.run(days=dt)
             if i < 1:
                 m.params.max_ts *= 1.5
                 m.params.first_ts = m.params.max_ts
@@ -207,21 +75,21 @@ def run_simulation(domain: str, max_ts: float, nx: int = 100, mesh_filename: str
     elif domain == '2D':
         plot(m=m, ith_step=ith_step)
         ith_step += 1
-        run(self=m, days=0.001, restart_dt=max_ts)
+        m.run(days=0.001, restart_dt=max_ts)
         plot(m=m, ith_step=ith_step)
         ith_step += 1
         m.params.max_ts *= 40
         m.params.first_ts = m.params.max_ts
-        run(self=m, days=0.001)
+        m.run(days=0.001)
         plot(m=m, ith_step=ith_step)
         ith_step += 1
-        run(self=m, days=0.001)
+        m.run(days=0.001)
         plot(m=m, ith_step=ith_step)
         ith_step += 1
 
         for i in range(15):
             dt = 0.4
-            run(self=m, days=dt)
+            m.run(days=dt)
             if i < 1:
                 m.params.max_ts *= 1.5
                 m.params.first_ts = m.params.max_ts
@@ -230,21 +98,21 @@ def run_simulation(domain: str, max_ts: float, nx: int = 100, mesh_filename: str
     elif domain == '3D':
         plot(m=m, ith_step=ith_step)
         ith_step += 1
-        run(self=m, days=0.001, restart_dt=max_ts)
+        m.run(days=0.001, restart_dt=max_ts)
         plot(m=m, ith_step=ith_step)
         ith_step += 1
         m.params.max_ts *= 20
         m.params.first_ts = m.params.max_ts
-        run(self=m, days=0.001)
+        m.run(days=0.001)
         plot(m=m, ith_step=ith_step)
         ith_step += 1
-        run(self=m, days=0.001)
+        m.run(days=0.001)
         plot(m=m, ith_step=ith_step)
         ith_step += 1
 
         for i in range(15):
             dt = 0.4
-            run(self=m, days=dt)
+            m.run(days=dt)
             if i < 1:
                 m.params.max_ts *= 1.5
                 m.params.first_ts = m.params.max_ts
@@ -267,23 +135,26 @@ if __name__ == '__main__':
     # run_simulation(domain='2D', nx=100, max_ts=8.e-4, output=True, poro_filename='input/spherical_100_8.txt')
 
     # 3D
-    # run_simulation(domain='3D', max_ts=2.e-3, output=True,
+    # run_simulation(domain='3D', max_ts=2.e-3, output=False,
     #                mesh_filename='input/core_13k.msh', poro_filename='input/core_13k_0.02.txt')
     # run_simulation(domain='3D', max_ts=1.e-3, output=True,
     #                mesh_filename='input/core_60k.msh', poro_filename='input/core_60k_0.01.txt')
+    # run_simulation(domain='3D', max_ts=8.e-4, output=True,
+    #                mesh_filename='input/core_195k.msh', poro_filename='input/core_195k.txt')
+
 
 # paths = ['./100x100/data_ts3.vts',
-#          './100x100/data_ts14.vts']
-# write_2d_output_for_paper(paths=paths)
-# paths = ['output_200/log.txt', 'output_2000_50000/log.txt',
-#          'output_200_1000_5/log.txt', 'output_500/log.txt',
-#          'output_200_1000_no_reaction/log.txt']
-# labels = [r'$n_x=200,\, \Delta t_{max}=10^{-3}$ day, $n_{obl}=5001$',
-#           r'$n_x=200,\, \Delta t_{max}=1\cdot 10^{-4}$ day, $n_{obl}=50001$',
-#           r'$n_x=200,\, \Delta t_{max}=5\cdot 10^{-3}$ day, $n_{obl}=1001$',
-#           r'$n_x=500,\, \Delta t_{max}=5\cdot 10^{-4}$ day \, $n_{obl}=5001$',
-#           r'$n_x=200,\, \Delta t_{max}=8\cdot 10^{-6}$ day, $n_{obl}=5001$, no reaction']
-# linestyle = ['-', '-.', ':', '-', '--']
-# colors=['b', 'b', 'b', 'r', 'b']
-# nx = [200, 200, 200, 500, 200]
-# plot_max_cfl(paths=paths, labels=labels, nx=nx, linestyle=linestyle, colors=colors)
+    #          './100x100/data_ts14.vts']
+    # write_2d_output_for_paper(paths=paths)
+    # paths = ['output_200/log.txt', 'output_2000_50000/log.txt',
+    #          'output_200_1000_5/log.txt', 'output_500/log.txt',
+    #          'output_200_1000_no_reaction/log.txt']
+    # labels = [r'$n_x=200,\, \Delta t_{max}=10^{-3}$ day, $n_{obl}=5001$',
+    #           r'$n_x=200,\, \Delta t_{max}=1\cdot 10^{-4}$ day, $n_{obl}=50001$',
+    #           r'$n_x=200,\, \Delta t_{max}=5\cdot 10^{-3}$ day, $n_{obl}=1001$',
+    #           r'$n_x=500,\, \Delta t_{max}=5\cdot 10^{-4}$ day \, $n_{obl}=5001$',
+    #           r'$n_x=200,\, \Delta t_{max}=8\cdot 10^{-6}$ day, $n_{obl}=5001$, no reaction']
+    # linestyle = ['-', '-.', ':', '-', '--']
+    # colors=['b', 'b', 'b', 'r', 'b']
+    # nx = [200, 200, 200, 500, 200]
+    # plot_max_cfl(paths=paths, labels=labels, nx=nx, linestyle=linestyle, colors=colors)
